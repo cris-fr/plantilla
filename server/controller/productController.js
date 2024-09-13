aa6afe90c59bfafcd11f70a97b1eb47bf78490ae
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-09-13 08:21:59 -0500

    feat: :sparkles: Find MVC

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -14,3 +14,15 @@ exports.save = async (req, res) => {
     }
 }
 
+exports.search = async (req, res) => {
+    try {
+        let data = await product.findCollection();
+        res.status(200).json({status: 200, data});
+    } catch (error) {
+        let err = JSON.parse(error.message);
+        if(err.status == 500) return res.status(err.status).json(err);
+
+        return error;
+    }
+}
+
--- server/version/productV1.js
+++ server/version/productV1.js
@@ -3,7 +3,7 @@ const express = require('express');
 const product = express();
 
 product.post("/", express.json(), controlerProduct.save);
-//product.get("/", controlerProduct.search)
+product.get("/", controlerProduct.search)
 //product.delete("/:id", controlerProduct.delete)
 //product.update("/:id", express.json(), controlerProduct.update);
 
